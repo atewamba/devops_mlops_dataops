59a7ff5f8367549be5bb7b1161a6c6b7b6fb240f
Calvin Atewamba <calvin.atewamba@gainx.com>
2023-11-04 00:02:56 -0400

    adjust fastapi_test.py

--- fastapi_test.py
+++ fastapi_test.py
@@ -15,20 +15,26 @@ def read_root():
     return {"Hello": "World"}
 
 
+'''
 class Item(BaseModel):
     name: str
     price: float
     is_offer: bool = None
+'''
 
 
-@app.get("/items/{item_id}")
+
+
+@app.get("/items/{item_id}/{q}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
+'''
 @app.put("/items/{item_id}")
 def save_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_price": item.price, "item_id": item_id}
+'''
 
 
 if __name__ == "__main__":
