5a5b7a59fb005ffce6b4ef75de315aa2cf3b6267
Calvin Atewamba <calvin.atewamba@gainx.com>
2023-11-04 16:13:14 -0400

    add fastapi_test.py

--- fastapi_test.py
+++ fastapi_test.py
@@ -4,8 +4,7 @@ Try FastAPI
 
 from typing import Union
 from fastapi import FastAPI
-
-# from pydantic import BaseModel
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
@@ -15,11 +14,19 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
 
 @app.get("/items/{item_id}/{q}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.put("/items/{item_id}")
+def save_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_price": item.price, "item_id": item_id}
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8080, host="127.0.0.1")
+    uvicorn.run(app, port=8080, host="0.0.0.0")
